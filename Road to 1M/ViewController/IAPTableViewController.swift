b8956eaae2ba8cf840b22137be0b371fa3f95327
Vinayak <buggyman026@gmail.com>
2018-08-03 22:35:16 +0530

    Changes

--- Road to 1M/ViewController/IAPTableViewController.swift
+++ Road to 1M/ViewController/IAPTableViewController.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-class IAPTableViewController: UITableViewController {
-
+class IAPTableViewController: UIViewController {
+    @IBOutlet weak var tableView:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
          IAPService.shared.getProducts()
-    }
-
-override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    switch indexPath.row {
-    case 0: IAPService.shared.purchase(product: .nonConsumable)
-    case 1: IAPService.shared.purchase(product: .autoRenewingSubsciption)
-    default: print("Restore")
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
+
+extension IAPTableViewController:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellPre")
+        if(indexPath.row == 0){
+            cell?.textLabel?.text = "Premium (Beta)"
+        }else if(indexPath.row == 1){
+            cell?.textLabel?.text = "Premium Membership"
+        }else{
+            cell?.textLabel?.text = ""
+        }
+        return cell!
+    }
+    
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0: IAPService.shared.purchase(product: .nonConsumable)
+        case 1: IAPService.shared.purchase(product: .autoRenewingSubsciption)
+        default: print("Restore")
+        }
+    }
+    
 }
